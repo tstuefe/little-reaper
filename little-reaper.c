ad79b238ce33cf4fdfdf01efd4190862078ec00f
tstuefe <thomas.stuefe@gmail.com>
2022-04-08 18:52:09 +0200

    reaper

--- little-reaper.c
+++ little-reaper.c
@@ -20,6 +20,7 @@
 
 
 
+#include <assert.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,9 +31,17 @@
 #include <unistd.h>
 #include <sys/prctl.h>
 
+
 static int verbose = 0;
 static int fault_tolerant = 0;
-static int dont_quit = 0;
+static int dont_reap = 0;
+
+// This defines how long the process should run:
+// per default we terminate when the direct child (the command we spawned) terminates
+// if -w, we wait until all sub processes quit, which includes the command
+// if -W, we wait forever
+static int wait_for_all_sub_processes = 0;
+static int wait_forever = 0;
 
 static void LOG(const char* msg) {
 	printf("%s\n", msg);
@@ -48,12 +57,17 @@ static void LOGf(const char* fmt, ...) {
 	fflush(stdout);
 }
 
+#define VERBOSEf(fmt, ...) 	if (verbose) { LOGf(fmt, __VA_ARGS__); }
+#define VERBOSE(msg) 		if (verbose) { LOG(msg); }
+
 static void print_usage() {
 	LOG("Usage: little-reaper [options] <command> [<command arguments> ...]");
 	LOG("       options:");
 	LOG("       -v: verbose mode");
 	LOG("       -f: fault tolerant mode");
-	LOG("       -w: start command, but don't quit when its finished");
+	LOG("       -w: Wait for all child processes (default: only wait for command)");
+	LOG("       -W: Wait forever, never finish");
+	LOG("       -r: Dont reap child processes");
 }
 
 // Make process a sub reaper for all direct and indirect children
@@ -65,6 +79,20 @@ static void make_me_a_reaper() {
 			exit(-1);
 		}
 	}
+}
+
+static int return_code_from_process_status(int status) {
+	if (WIFSIGNALED(status)) {
+		return -1;
+	} else if (WIFEXITED(status)) {
+		return WEXITSTATUS(status);
+	}
+	return -1;
+}
+
+static void infinite_sleep() {
+	VERBOSE("sleeping forever...");
+	for (;;) sleep(10);
 }
 
 int main(int argc, char** argv) {
@@ -80,6 +108,7 @@ int main(int argc, char** argv) {
 				exit(-1);
 			}
 			for (int letter = 1; letter < len; letter ++) {
+				VERBOSEf("read flag: %c", argv[i][letter]);
 				switch (argv[i][letter]) {
 				case 'v': 
 					verbose = 1;
@@ -88,7 +117,13 @@ int main(int argc, char** argv) {
 					fault_tolerant = 1;
 					break;
 				case 'w':
-					dont_quit = 1;
+					wait_for_all_sub_processes = 1;
+					break;
+				case 'W':
+					wait_forever = 1;
+					break;
+				case 'r':
+					dont_reap = 1;
 					break;
 				default: 
 					LOGf("Unknown flag: %c", argv[i][letter]);
@@ -118,19 +153,15 @@ int main(int argc, char** argv) {
 	}
 	child_argv[child_argc] = NULL;
 
-	if (verbose) {
-		LOGf("little-reaper (pid: %d, parent: %d)", getpid(), getppid());
-		for (int i = 0; child_argv[i] != NULL; i ++) {
-			LOGf("argv[%d]: \"%s\"", i, child_argv[i]);
-		}
-		LOGf("verbose: %d", verbose);
-		LOGf("fault_tolerant: %d", 1);
-		LOGf("dont_quit: %d", dont_quit);
-	}
+	VERBOSEf("little-reaper (pid: %d, parent: %d)", getpid(), getppid());
+//	for (int i = 0; child_argv[i] != NULL; i ++) {
+//		VERBOSEf("argv[%d]: \"%s\"", i, child_argv[i]);
+//	}
 
 	// fork, then exec the child process
-	pid_t child = fork();
-	if (child == 0) {
+	pid_t command = fork();
+
+	if (command == 0) {
 		int rc = execv(child_argv[0], child_argv);
 		if (rc == -1) {
 			LOGf("Failed to exec \"%s\" - errno: %d (%s)",
@@ -138,14 +169,62 @@ int main(int argc, char** argv) {
 			exit(-1);
 		}
 	} else {
-		int status;
-		pid_t p = waitpid(child, &status, 0);
-		if (dont_quit) {
-			for (;;) {
-				sleep(120);
+
+		// If we were asked not to reap, we cannot wait; we either wait forever or quit immediately.
+		if (dont_reap) {
+			if (wait_forever) {
+				infinite_sleep();
+			}
+			exit(0);
+		}
+
+		int command_status = 0;
+		int command_terminated = 0;
+		int no_child_left = 0;
+		for (;;) {
+			int status;
+			pid_t child = wait(&status);
+			if (child > 0) {
+				if (WIFEXITED(status) || WIFSIGNALED(status)) {
+					if (WIFEXITED(status)) {
+						VERBOSEf("child %d exited with %d", child, WEXITSTATUS(status));
+					}
+					if (WIFSIGNALED(status)) {
+						VERBOSEf("child %d terminated with signal %d", child, WTERMSIG(status));
+					}
+					if (child == command) {
+						VERBOSE("Command finished.");
+						command_status = status;
+						command_terminated = 1;
+					}
+				}
+			} else {
+				if (errno == ECHILD) {
+					no_child_left = 1;
+					VERBOSE("All child processes finished");
+				} else {
+					VERBOSEf("wait error (%d %s)", errno, strerror(errno));
+				}
+			}
+
+			if (wait_forever) {
+				if (no_child_left) {
+					infinite_sleep();
+				}
+			} else {
+				if (wait_for_all_sub_processes) {
+					if (no_child_left) {
+						assert(command_terminated);
+						exit(return_code_from_process_status(command_status));
+					}
+				} else {
+					if (command_terminated) {
+						exit(return_code_from_process_status(command_status));
+					}
+				}
 			}
 		}
 	}
-
-	exit(0);
+	
+	return 0;
 }
